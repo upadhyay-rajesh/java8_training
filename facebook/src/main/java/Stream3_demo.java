d34ba07297b006db609927fd29cf6a224daa55cb
rajesh <upadhyay.rajesh@rediffmail.com>
2020-05-27 15:40:20 +0530

    hjg

--- facebook/src/main/java/Stream3_demo.java
+++ facebook/src/main/java/Stream3_demo.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Stream3_demo {
 
 	public static void main(String[] args) {
-		Employee.persons().stream()
-		.filter(Employee::isFemale)
+		/*Employee.persons().stream()
+		.filter(Employee::isFemale)   //return all female employee
+		
 				.map(Employee::getName)
 				.forEach(System.out::println);
 		
@@ -22,7 +30,59 @@ public class Stream3_demo {
 		
 		long ll=Employee.persons().stream().count();
 		System.out.println(ll);
+		*/
+		
+		List<String> name=Employee.persons().stream()
+				.map(Employee::getName)
+				.collect(ArrayList::new,ArrayList::add,ArrayList::addAll);
+		
+		List<String> name1=Employee.persons().stream()
+				.map(Employee::getName)
+				.collect(Collectors.toList());
+		System.out.println(name1);
+		
+		
+		SortedSet<String> name2=Employee.persons().stream()
+				.map(Employee::getName)
+				.collect(Collectors.toCollection(TreeSet::new));
+		System.out.println(name2);
+		
+		DoubleSummaryStatistics ds=Employee.persons().stream()
+		 .collect(Collectors.summarizingDouble(Employee::getIncome));
+		
+		System.out.println("income summary "+ds);
+		
+		Map<Long, String> mm=Employee.persons().stream()
+		.collect(Collectors.toMap(Employee::getId,Employee::getName));
+		
+		System.out.println(mm);
+		
+		//{male= {a,c,d,f}, female= {s,d,c}}
+		
+		Map<Employee.Gender, String> mm1=Employee.persons().stream() //return 7 record
+				.collect(Collectors.toMap(Employee::getGender,Employee::getName,
+						(oldValue,newValue)->String.join(", ", oldValue,newValue)
+						));
+				
+				System.out.println(mm1);
+		//
+		
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
